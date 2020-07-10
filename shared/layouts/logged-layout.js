b64da81917508f12043647ca7b11d3eaa1533f2f
bryancua <48697038+bryancua@users.noreply.github.com>
2020-07-10 14:23:44 -0700

    feature: mock server (#1)
    Co-authored-by: Bryan Cua <bryan@luxe.technology>

--- shared/layouts/logged-layout.js
+++ shared/layouts/logged-layout.js
@@ -1,10 +1,26 @@
 import "./_logged-layout.less"
 import Header from "./header"
 import Footer from "./footer"
+import call from "~/server"
+import {go} from "~/shared/utils/routes"
+import React, {useEffect} from "react"
 
 export default function LoggedLayout({
 	children
 }) {
+
+	async function me(){
+		try{
+			const me = await call("GET", "me")
+		}catch(err){
+			go("/")
+		}
+	}
+
+	useEffect(()=>{
+		me()
+	}, [])
+
 	return (
 		<div className="logged-layout">
 			<Header title="Monstercat - Welcome" />
